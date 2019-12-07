26a6509a2b1bafe427099f67728f1a465c2f249b
hamzarafiq21212 <52358886+hamzarafiq21212@users.noreply.github.com>
2019-12-08 00:20:32 +0500

    Update app.js

--- src/redux/actionTypes/app.js
+++ src/redux/actionTypes/app.js
@@ -1,7 +1,2 @@
 export const POMODORO = 'POMODORO';
 export const SHORT_BREAK = 'SHORT_BREAK';
-
-
-
-// export const OPEN_SNACKBAR = 'OPEN_SNACKBAR';
-// export const CLOSE_SNACKBAR = 'CLOSE_SNACKBAR';
